353604c43a6c085682e4eeba6219bcad9b9148e6
FelipeRangel77 <felipealbuquerquerangel@gmail.com>
2026-02-14 18:25:24 -0300

    update

---
+++ Estudos Automos/Basico/numeros.py
@@ -0,0 +1,22 @@
+
+print(type(1))
+print(type(-1))
+print(type(1.1))
+print(type('Otavio'))
+print(type(-1.1))
+print(type(0.0))
+
+# Tipo de dado bool (boolean)
+# Ao questionar algo em um programa,
+# só existem duas respostas possíveis.
+# sim(true), nao (false).
+# Existem vasrios operadores para "questionar"
+# dentre eles o ==. que é um operador logico que
+# questiona se um valor é igual a outro
+
+print(10 == 10) # Sim => True
+print(10 == 11) # Não => False
+print(type(True)) 
+print(type (False)) 
+print(type(10 == 10))
+print(type(10 == 11)) 
