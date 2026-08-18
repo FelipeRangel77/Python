bc923244830556e6032591edc4bbc3fbe496d207
Felipe Rangel <felipealbuquerquerangel@gmail.com>
2026-08-18 08:56:54 -0300

    Update

---
+++ Estudos Automos/Basico/IMC.py
@@ -0,0 +1,12 @@
+nome = 'Felipe Rangel'
+altura = 1.65
+peso = 70
+imc = peso / altura ** 2
+
+print(nome, 'tem', altura, 'de altura,',)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
+
+# Felipe Rangel tem 1.65 de altura,
+# pesa 70 quilos e seu IMC é
+# 
